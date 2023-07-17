542e1a063764070e01bff28fc4879037e6a6b4f1
yongwen.long@fusionride.com <yongwen.long@fusionride.com>
2023-07-17 11:11:46 +0800

    this is first  submit of dev branch

--- CrossTrafficAlert/source/FakeApp.c
+++ CrossTrafficAlert/source/FakeApp.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "CrossTrafficAlert.h"
 //#include "object_tracker_types.h"
+//12132
 
 #pragma warning(disable : 4996)
 static void PlotInterface(FILE* gnupipe, trackList_t* p_InputTrackList, CTA_ClassMembers_t* p_thisMember);
